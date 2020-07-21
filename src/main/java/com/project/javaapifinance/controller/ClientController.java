4bd14a9ae5daab1b17d32ee41d1e66088aa823e3
Renan Lemos <renan_lemos@outlook.com>
2020-07-20 23:49:25 -0300

    Implementing http methods for ClientController and unit tests

--- src/main/java/com/project/javaapifinance/controller/ClientController.java
+++ src/main/java/com/project/javaapifinance/controller/ClientController.java
@@ -4,8 +4,16 @@ import java.util.List;
 import com.project.javaapifinance.domain.Client;
 import com.project.javaapifinance.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +26,28 @@ public class ClientController {
     @GetMapping("/")
     public List<Client> getAllClients() {
         return clientService.findAllClients();
+    }
+
+    @GetMapping("/{id}")
+    public Client getClientById(@PathVariable Long id) {
+        return clientService.getClientById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Client createClient(@RequestBody Client client) {
+        return clientService.saveClient(client);
+    }
+
+    @PutMapping
+    public Client updateClient(@RequestBody Client client) {
+        return clientService.updateClient(client);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteClient(@PathVariable Long id) {
+        clientService.deleteClientById(id);
     }
     
 }
--- src/test/java/com/project/javaapifinance/controller/ClientControllerTests.java
+++ src/test/java/com/project/javaapifinance/controller/ClientControllerTests.java
@@ -2,13 +2,20 @@ package com.project.javaapifinance.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
+import com.project.javaapifinance.builder.ClientCreator;
+import com.project.javaapifinance.domain.Client;
 import com.project.javaapifinance.service.ClientService;
+import com.project.javaapifinance.utils.JsonUtils;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = ClientController.class)
@@ -22,6 +29,35 @@ public class ClientControllerTests {
 
     @Test
     public void mustReturn200WhenGetClients() throws Exception {
-        this.mockMvc.perform(get("/")).andExpect(status().isOk());
+        this.mockMvc.perform(get("/client/")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void mustReturn200WhenGetAClientById() throws Exception {
+        this.mockMvc.perform(get("/client/1")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void mustReturn201WhenCreateAClient() throws Exception {
+        Client client = new ClientCreator().createClientWithoutId();
+        String body = JsonUtils.jsonString(client);
+
+        this.mockMvc.perform(post("/client/").content(body).contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void mustReturn200WhenUpdateAClient() throws Exception {
+        Client client = new ClientCreator().createClientWithId();
+        String body = JsonUtils.jsonString(client);
+
+        this.mockMvc.perform(put("/client/").content(body).contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+    }
+
+    @Test
+    public void mustReturn204WhenDeleteAClient() throws Exception {
+        this.mockMvc.perform(delete("/client/1"))
+                    .andExpect(status().isNoContent());
     }
 }
